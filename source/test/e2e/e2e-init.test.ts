cb5f6c79f2c092e4016e0b3278b416b9ae3ddfdc
Jonatan Lampa <jonatan.lampa@scania.com>
2026-05-21 10:33:06 +0200

    fix assertions

--- source/test/e2e/e2e-init.test.ts
+++ source/test/e2e/e2e-init.test.ts
@@ -17,13 +17,13 @@ const configureFreshInstall = async (tui: ReturnType<typeof setupTui>) => {
 
 	output = await tui.waitFor('Type  :config editor');
 
-	expect(output).toContain('choose your preferred editor');
+	expect(output).toContain('pick your editor');
 
 	tui.input(':config editor vim\r');
 
 	output = await tui.waitFor('Type  :config autoSync');
 
-	expect(output).toContain('choose whether to enable auto-sync');
+	expect(output).toContain('Configure auto sync');
 
 	tui.input(':config autoSync on\r');
 
